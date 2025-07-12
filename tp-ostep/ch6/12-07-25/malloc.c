ef306dfa8bde91657ff58e4316aec7acdf887298
tonyjustdevs <tonyjustdevs@gmail.com>
2025-07-12 17:17:05 +1000

    add 120725 part1 scanf of malloc.c

---
+++ tp-ostep/ch6/12-07-25/malloc.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+
+
+int main(){
+
+    // 1. ask user # grades (single chars) they will enter
+    int user_chosen_no_of_grades;
+
+    printf("Please enter number of Grades: ");
+    scanf("%d",&user_chosen_no_of_grades);
+
+    printf("user chose nbr of grades: [%d]\n",user_chosen_no_of_grades);
+    // 2. calculate required memory
+    // int grades[nbr_of_grades]=; // ?? malloc??
+    
+    
+    // 3. allocate memory
+    
+    // 4. accept user input
+    
+    // 5. print user input
+    
+    // 6. free memory?
+    
+    
+    return 0;
+}
+
+
