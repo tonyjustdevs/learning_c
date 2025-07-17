8975b00322627b6c252759b33519236896edd631
tonyjustdevs <tonyjustdevs@gmail.com>
2025-07-17 18:41:35 +1000

    add cmd_args_v3 with error check

--- tp-ostep/ch6/17-07-25/cmd_args_v3.c
+++ tp-ostep/ch6/17-07-25/cmd_args_v3.c
@@ -1,7 +1,26 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
 
 int main(int argc, char* argv[]){
-    printf("G'day");
+    // printf("G'day");
+
+    // exit early if no args
+
+    if (argc==1)
+    {
+        // perror("[tp] Input error: ");
+        fprintf(stderr,"%d\n", argc); 
+        // fprintf(stderr,"%d\n", stderr(errno)); 
+        // fprintf(stderr,"%d\n", stderr(errno)); 
+        // fprintf(stderr, "Error code: %d - %s\n", errno, strerror(errno));
+        // fprintf(stderr, "Error code: %d - %s\n", errno, stderr(errno));
+
+        
+        return 1;
+    }
+    
+
     for (int i = 1; i < argc; i++)
     {
         printf(" %s",argv[i]);
